8f162f48680de0c91af033fb84215f1219dcc519
Nguyễn Hoàng Lâm <165152517+Kuren20052002@users.noreply.github.com>
2024-05-02 12:15:31 +0700

    Add a function to compare user and computer choice in im-cooking.js

--- im-cooking.js
+++ im-cooking.js
@@ -11,6 +11,14 @@ function getComputerChoice(){
     }
 }
 
+function pointResult(user_choice, computer_choice){
+    if(user_choice === computer_choice) return 0;
+    else if(user_choice === "rock" && computer_choice === "scissors") return 1;
+    else if(user_choice === "paper" && computer_choice === "rock") return 1;
+    else if(user_choice === "scissors" && computer_choice === "paper") return 1;
+    else return -1;
+}
+
 
 
 
